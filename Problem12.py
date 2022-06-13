4515ef97c8a66183f5f30e3c1974f5f2d60154bc
MakarKad <makarkadobanskij@gmail.com>
2022-06-13 14:14:45 +0300

    commit

--- Problem12.py
+++ Problem12.py
@@ -1,2 +1,22 @@
-a = 
-for i in a:
+def dil(m):
+    b = 1
+    for i in range(1, m + 1):
+        if i > m:
+            break
+        if m % i == 0:
+            b += 1
+            m = int(m / i)
+    return b
+
+
+b = 0
+c = 0
+a = 1
+while b <= 500:
+    b = 0
+    c += a
+    b = dil(c)
+    a += 1
+print(b,c)
+
+
